e2deb7d3dc436e4ea69123d1edd58cbad3aaa075
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-08-17 21:49:45 +0200

    lint with standard js

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'getCustomPatterns') {
     chrome.storage.sync.get(['customPatterns'], (result) => {
-      sendResponse({ customPatterns: result.customPatterns });
-    });
-    return true; // Keep message channel open for async response
+      sendResponse({ customPatterns: result.customPatterns })
+    })
+    return true // Keep message channel open for async response
   }
-});
+})
